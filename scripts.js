58beef4e902cd2d0706ec9e79f151aced5a700d9
Michael Pearson <47512372+Michael-117@users.noreply.github.com>
2021-10-24 21:39:00 -0500

    Javascript Added

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
-/* Add your JavaScript to this file */
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    var message = document.getElementsByClassName("message");
+    var button = document.querySelector("button");
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    button.onclick = function (event) {
+        email = document.querySelector("input").value;
+
+        if (email.match(mailformat)) {
+            message[0].innerHTML = "Thank you! Your email address " + email + " has been added to our mailing list!";
+            event.preventDefault();
+        } 
+        else {
+            message[0].innerHTML = "Please enter a valid email address.";
+            event.preventDefault();
+        }
+    };
+})
